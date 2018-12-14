daec2b23d7b14bb5277095bdf994d883d9ae0ba7
DustyShap <dustinshapiro@gmail.com>
2018-12-14 16:44:30 -0600

    Sockets

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 from thinkaboutit import app, routes
 
 if __name__ == '__main__':
-    app.run()
+    socketio.run(app, debug=True)
--- thinkaboutit/routes.py
+++ thinkaboutit/routes.py
@@ -1,6 +1,18 @@
 from flask import render_template
 from thinkaboutit import app
+from flask_socketio import SocketIO, join_room, emit
+
+
+
+socketio = SocketIO(app)
+ROOMS = {} # dict to track active rooms
 
 @app.route('/')
 def index():
-    return 'Hello'
+    return render_template('index.html')
+
+@socketio.on('create')
+def on_create(data):
+    """Create a game lobby"""
+    print(data)
+    emit('join_room', {'room': 'hi'})
