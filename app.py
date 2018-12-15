c24c8a3a275c8185aed8827a6979e8fa9dde6150
DustyShap <dustinshapiro@gmail.com>
2018-12-14 22:54:46 -0600

    sockets

--- app.py
+++ app.py
@@ -1,4 +1,21 @@
-from thinkaboutit import app, routes
+from flask import render_template
+from flask_socketio import SocketIO, join_room, emit
+from thinkaboutit import app
+import os
+
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+socketio = SocketIO(app)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('create')
+def on_create(data):
+    """Create a game lobby"""
+    print(data)
+    emit('join_room', {'room': 'hi'})
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
